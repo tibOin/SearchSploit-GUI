e5c284f312de8ba361b2ddc3628caa9f556a41ab
tiboin <tiboin-boin@hotmail.fr>
2018-03-16 03:17:03 +0100

    Initial Commit

---
+++ SearchSploit GUI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SearchSploit GUI
+//
+//  Created by Anaelle Saint-Jalm on 16/03/2018.
+//  Copyright © 2018 Home. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SearchSploit GUI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SearchSploit GUI
+//
+//  Created by Anaelle Saint-Jalm on 16/03/2018.
+//  Copyright © 2018 Home. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
